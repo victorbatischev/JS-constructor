974ac99bd9830ff525a9e2a7289ef5dee85a03e9
Victor <vitek.batishev97@gmail.com>
2020-10-31 11:19:50 +0300

    добавил скрипт генерации кода

--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
 import './styles/main.css'
+import { model } from './model.js'
+
+console.log(model)
+
+let generateHTML = (model) => {
+  let htmlBlock
+  return model
+    .map((block) => {
+      switch (block.type) {
+        case 'title':
+          htmlBlock = convertModelToTitle(block.value)
+          break
+        case 'text':
+          htmlBlock = convertModelToText(block.value)
+          break
+        case 'textColumns':
+          htmlBlock = convertModelToColumns(block.value)
+          break
+        default:
+          htmlBlock = `<br>`
+          break
+      }
+      return htmlBlock
+    })
+    .join('')
+}
+
+let convertModelToTitle = (value) => {
+  return `
+    <div class="row">
+      <div class="col-sm">
+        <h1>${value}</h1>
+      </div>
+    </div>`
+}
+
+let convertModelToText = (value) => {
+  return `
+    <div class="row">
+      <div class="col-sm">
+        <p>
+          ${value}
+        </p>
+      </div>
+    </div>`
+}
+
+let convertModelToColumns = (value) => {
+  let columns = value
+    .map((item) => {
+      return `
+      <div class="col-sm">
+        <p>
+          ${item}
+        </p>
+      </div>`
+    })
+    .join('')
+  return `
+    <div class="row">
+      ${columns}
+    </div>`
+}
+
+document
+  .querySelector('#site')
+  .insertAdjacentHTML('beforeend', generateHTML(model))
---
+++ src/model.js
@@ -0,0 +1,30 @@
+export const model = [
+  {
+    type: 'title',
+    value: 'Test title',
+    options: {}
+  },
+  {
+    type: 'text',
+    value: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    options: {}
+  },
+  {
+    type: 'textColumns',
+    value: [
+      `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
+      consectetur reprehenderit ex blanditiis assumenda quaerat ea atque
+      fugit eaque asperiores eligendi accusantium ratione exercitationem
+      delectus eveniet unde rerum, possimus voluptate!`,
+      `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
+      consectetur reprehenderit ex blanditiis assumenda quaerat ea atque
+      fugit eaque asperiores eligendi accusantium ratione exercitationem
+      delectus eveniet unde rerum, possimus voluptate!`,
+      `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
+      consectetur reprehenderit ex blanditiis assumenda quaerat ea atque
+      fugit eaque asperiores eligendi accusantium ratione exercitationem
+      delectus eveniet unde rerum, possimus voluptate!`
+    ],
+    options: {}
+  }
+]

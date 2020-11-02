1534f883f3a6953b30c956210a69d246d5f8a3c7
Victor <vitek.batishev97@gmail.com>
2020-11-02 16:38:57 +0300

    firebase deploy

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { model } from './model.js'
 import { Site } from './classes/site.js'
 import { SideBar } from './classes/sidebar.js'
 
+const site = new Site('#site')
+
 const updateCallback = (newBlock) => {
   model.push(newBlock)
   site.render(model)
 }
 
-new Site('#site').render(model)
+site.render(model)
 new SideBar('#panel', updateCallback)
